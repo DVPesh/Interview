0107c9d6dc5d4445b861e987daa9ad57492f4686
dvpesh <dvpesh@mail.ru>
2023-01-24 17:27:11 +0300

    Реализованы основные методы ArrayList

--- Collections/src/main/java/ru/peshekhonov/Main.java
+++ Collections/src/main/java/ru/peshekhonov/Main.java
@@ -1,8 +1,10 @@
 package ru.peshekhonov;
 
+import ru.peshekhonov.list.array.ArrayList;
 import ru.peshekhonov.list.linked.LinkedList;
 
 public class Main {
+
     public static void main(String[] args) {
         LinkedList<String> stringLinkedList = new LinkedList<>();
         stringLinkedList.add("First");
@@ -33,5 +35,69 @@ public class Main {
 
         System.out.println(doubleLinkedList.remove(2.0));
         System.out.println(doubleLinkedList);
+
+        ArrayList<Float> floatArrayList = new ArrayList<>();
+        System.out.println(floatArrayList);
+
+        ArrayList<String> stringArrayList = new ArrayList<>(2);
+        System.out.println(stringArrayList);
+
+        stringArrayList.add("First");
+        stringArrayList.add("Second");
+        System.out.println(stringArrayList);
+
+        stringArrayList.add(2, "Third");
+        System.out.println(stringArrayList);
+        stringArrayList.add("Fourth");
+        System.out.println(stringArrayList);
+        stringArrayList.add("Fifth");
+        System.out.println(stringArrayList);
+
+        floatArrayList.add(2f);
+        floatArrayList.add(1, 3f);
+        floatArrayList.add(4f);
+        floatArrayList.add(0, 1f);
+        System.out.println(floatArrayList);
+
+        System.out.println(floatArrayList.get(0));
+        System.out.println(floatArrayList.get(1));
+        System.out.println(floatArrayList.get(2));
+        System.out.println(floatArrayList.get(3));
+
+        System.out.println(floatArrayList.remove(5f));
+        System.out.println(floatArrayList);
+
+        System.out.println(floatArrayList.remove(3f));
+        System.out.println(floatArrayList);
+
+        System.out.println(floatArrayList.remove(1));
+        System.out.println(floatArrayList);
+
+        System.out.println(floatArrayList.remove(0));
+        System.out.println(floatArrayList);
+
+        for (float value = 5f; value < 20f; value++) {
+            floatArrayList.add(value);
+            System.out.println(floatArrayList);
+        }
+
+        ArrayList arrayList = new ArrayList<>();
+        System.out.println(arrayList);
+        for (int i = -5; i < 6; i++) {
+            arrayList.add(i);
+        }
+        System.out.println(arrayList);
+
+        arrayList.add(0, -6);
+        System.out.println(arrayList);
+
+        System.out.println(arrayList.remove(4));
+        System.out.println(arrayList);
+        System.out.println(arrayList.remove(Integer.valueOf(4)));
+        System.out.println(arrayList);
+        System.out.println(arrayList.get(4));
+
+        System.out.println(stringArrayList);
+        System.out.println(floatArrayList);
     }
 }
