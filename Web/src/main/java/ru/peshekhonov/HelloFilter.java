d88f80cee8e4422a017139c758aeb6dcfc2025e6
dvpesh <dvpesh@mail.ru>
2023-02-09 20:35:37 +0300

    javax.servlet заменён на jakarta.servlet. jakarta.servlet работает с apache-tomcat-10.0.27, а javax.servlet нет.

--- Web/src/main/java/ru/peshekhonov/HelloFilter.java
+++ Web/src/main/java/ru/peshekhonov/HelloFilter.java
@@ -1,18 +1,12 @@
 package ru.peshekhonov;
 
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
 import java.io.IOException;
 
 @WebFilter(filterName = "HelloFilter", urlPatterns = "/*")
 public class HelloFilter implements Filter {
-
-    public void init(FilterConfig config) throws ServletException {
-    }
-
-    public void destroy() {
-    }
-
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
         response.setContentType("text/html");
--- Web/src/main/java/ru/peshekhonov/HelloServlet.java
+++ Web/src/main/java/ru/peshekhonov/HelloServlet.java
@@ -1,9 +1,10 @@
 package ru.peshekhonov;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -13,10 +14,12 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter out = response.getWriter();
         out.printf("<!DOCTYPE html><html><head></head><body>");
-        out.println("<h1 style=\"color: purple;font-size:4em;\">Hello World</h1>");
+        out.println("<h1 style=\"color: purple;font-size:2em;\">Hello World</h1>");
         out.println("""
-                <p style="color: blue;font-size:3em;">
+                <p style="color: blue;font-size:1.5em;">
                 Курс "Подготовка к собеседованию Java-разработчика".
+                </p>
+                <p style="color: green;font-size:1.5em;">
                 Практичекое задание №6.
                 </p>
                 """);
