dc97b310cbf88807b950f4f991a4c0a327bcbb68
dvpesh <dvpesh@mail.ru>
2023-02-10 00:06:04 +0300

    добавлены скриншоты

--- Web/src/main/java/ru/peshekhonov/HelloServlet.java
+++ Web/src/main/java/ru/peshekhonov/HelloServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloServlet", urlPatterns = "/")
+@WebServlet(name = "HelloServlet", urlPatterns = "")
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
